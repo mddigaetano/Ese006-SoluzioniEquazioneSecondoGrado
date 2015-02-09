ad0f28c3e487d84e833c9bf6a87d6e0cf8bf0661
Amedeo Di Gaetano <amegro.agaty@gmail.com>
2015-02-09 09:58:57 +0100

    Update funzioni.c

--- funzioni.c
+++ funzioni.c
@@ -9,7 +9,7 @@ int esistonoSoluzioniReali(float a,float b,float c) {
 
 float calcolaDelta(float a,float b,float c) {
     float delta;
-    delta = (b*b)*(-4*a*c);
+    delta = (b*b)+(-4*a*c);
     return delta;
 }
 
